fab4ca52d1f54c056d7d4d4f02e742fd88a49bcc
Eric Zarycki <ericdzarycki@gmail.com>
2024-09-24 19:32:57 -0400

    Model Complete

--- main.R
+++ main.R
@@ -33,56 +33,40 @@ test <- read.csv("data/test.csv")
 colSums(is.na(train))
 colSums(is.na(test))
 
-#Exploratory Data Analysis (TBD)
-head(train)
-
-summary(train)
-
-# How many distinct variables are there?
-
-train %>% distinct()
-
-
-#Distributions of numeric variables
-# hist(train.numeric$model_year)
-# hist(train.numeric$milage)
-# hist(train.numeric$price, breaks="FD", xlim=c(0,3000000))
-# 
-# train.cat <-
-#   train %>% select(where(is.character))
-#Distributions of categorical variables
-# to do 
-# ggplot(train.cat, aes(y=reorder(brand, sum) sum)) + geom_bar()
-
-# Correlation matrix tbd
-# train.matrix <- cor(train)
-
 #########################Creating the Model##############################
 #Remove engine, transmission, fuel_type, model
-train.cleaned <- train %>% select(-engine,-transmission,-model,-id)
+train.cleaned <- train %>% dplyr::select(-engine,-transmission,-model,-id, -ext_col,-int_col)
 
-# Pre-processing 
+# Same for validation data
+test.cleaned <- test %>% dplyr::select(-engine,-transmission,-model,-id, -ext_col,-int_col)
 
-#Turn integers into numeric values
-train.numeric <- train.cleaned %>% mutate_if(is.integer, as.numeric)
-str(train.numeric)
+###########################One-hot encode############################
 
 # One-hot encode categorical variables
-train.onehot <- dummy_cols(train.numeric, select_columns = c("brand", "fuel_type", "ext_col", "int_col",
+train.onehot <- dummy_cols(train.cleaned, select_columns = c("brand", "fuel_type",
            "accident", "clean_title"))
 
-#Remove col names
+
+# One-hot encode categorical variables
+test.onehot <- dummy_cols(test.cleaned, select_columns = c("brand", "fuel_type", 
+                                                           "accident", "clean_title"))
+
+###############Remove Original Columns#########################
 train.onehot.cleaned <- 
   train.onehot %>%
-    select(-brand,-fuel_type,-ext_col, -int_col,
-           -accident, -clean_title)
+  dplyr::select(-brand,-fuel_type,
+                -accident, -clean_title, -brand_Polestar, -brand_smart)
 
-# make everything numeric because
+# Remove original categorical columns (same as training)
+test.onehot.cleaned <- test.onehot %>% dplyr::select(-brand, -fuel_type, -accident, -clean_title)
+
+#########################Make Sure everything is Numeric for Matrix#############
 
 train.onehot.ready <- train.onehot.cleaned %>% mutate_if(is.integer, as.numeric)
-# Feature Importance
+test.onehot.ready <- test.onehot.cleaned %>% mutate_if(is.integer, as.numeric)
 
-#split this into training and testing sets
+
+############################MODEL############################
 set.seed(88)
 train_indices <- sample(1:nrow(train.onehot.ready), size = 0.8 * nrow(train.onehot.ready))
 
@@ -93,6 +77,11 @@ val_data <- train.onehot.ready[-train_indices, ]
 train_labels <- train$price[train_indices]
 val_labels <- train$price[-train_indices]
 
+train_data <- train_data %>% select(-price)
+
+dtrain <- xgb.DMatrix(data = as.matrix(train_data), label = train_labels)
+dval <- xgb.DMatrix(data = as.matrix(val_data), label = val_labels)
+
 # Model
 bst <- xgboost(data = as.matrix(train_data),
                label = train_labels,
@@ -102,34 +91,10 @@ bst <- xgboost(data = as.matrix(train_data),
                verbose = 1)
 
 
+# Predict the prices for the validation set
+predictions <- predict(bst, as.matrix(test.onehot.cleaned))
 
-
-# Assuming you've done similar preprocessing for the test set:
-test.cleaned <- test %>% select(-engine, -transmission, -model, -id)
-
-# Turn integers into numeric values
-test.numeric <- test.cleaned %>% mutate_if(is.integer, as.numeric)
-
-# One-hot encode categorical variables
-test.onehot <- dummy_cols(test.numeric, select_columns = c("brand", "fuel_type", "ext_col", "int_col",
-                                                           "accident", "clean_title"))
-
-# Remove original categorical columns (same as training)
-test.onehot.cleaned <- test.onehot %>% select(-brand, -fuel_type, -ext_col, -int_col, -accident, -clean_title)
-
-# Ensure all data is numeric
-test.onehot.ready <- test.onehot.cleaned %>% mutate_if(is.integer, as.numeric)
-
-
-# Get the feature names from the test set
-test_features <- colnames(test.onehot.ready)
-
-# Identify missing columns in the test set
-missing_cols <- setdiff(train_features, test_features)
-
-
-
-
+##############SUBMISSION######################
 
 # Assuming your test set still contains the 'id' column
 submission <- data.frame(id = test$id, price = predictions)
